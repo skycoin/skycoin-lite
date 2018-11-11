a6c24ea3a6fad034bd72eef8c79b09c1b1f94c2d
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-11-11 08:46:13 -0400

    Create transactions with signatures

--- liteclient/client.go
+++ liteclient/client.go
@@ -3,6 +3,7 @@ package liteclient
 import (
 	"encoding/hex"
 	"encoding/json"
+	"log"
 
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/coin"
@@ -65,6 +66,32 @@ func GenerateAddresses(seed string, num int) []Address {
 // PrepareTransaction receives inputs and outputs and returns a signed transaction
 // inputsBody and outputsBody are JSONified arrays of TransactionInput and TransactionOutput, respectively.
 func PrepareTransaction(inputsBody string, outputsBody string) string {
+	newTransaction := buildTransaction(inputsBody, outputsBody, nil)
+	d := newTransaction.Serialize()
+
+	return hex.EncodeToString(d)
+}
+
+// PrepareTransactionWithSignatures receives inputs, outputs,and the signatures and returns.
+// inputsBody and outputsBody are JSONified arrays of TransactionInput and TransactionOutput, respectively.
+// signatureList is a JSONified array of strings.
+func PrepareTransactionWithSignatures(inputsBody string, outputsBody string, signatureList string) string {
+	var signatures []string
+	if err := json.Unmarshal([]byte(signatureList), &signatures); err != nil {
+		panic(err)
+	}
+
+	newTransaction := buildTransaction(inputsBody, outputsBody, signatures)
+	d := newTransaction.Serialize()
+
+	return hex.EncodeToString(d)
+}
+
+// Creates a coin.Transaction using the given lists of inputs, outputs and signatures. If signatureList is nill, the
+// signatures are created using the Secret property of each input.
+// inputsBody and outputsBody are JSONified arrays of TransactionInput and TransactionOutput, respectively.
+// signatureList is a JSONified array of strings.
+func buildTransaction(inputsBody string, outputsBody string, signatureList []string) coin.Transaction {
 	var inputs []TransactionInput
 	var outputs []TransactionOutput
 
@@ -81,9 +108,13 @@ func PrepareTransaction(inputsBody string, outputsBody string) string {
 	keys := make([]cipher.SecKey, len(inputs))
 
 	for i, in := range inputs {
-		k, err := cipher.SecKeyFromHex(in.Secret)
-		if err != nil {
-			panic(err)
+		if signatureList == nil {
+			k, err := cipher.SecKeyFromHex(in.Secret)
+			if err != nil {
+				panic(err)
+			}
+
+			keys[i] = k
 		}
 
 		inputHash, err := cipher.SHA256FromHex(in.Hash)
@@ -91,7 +122,6 @@ func PrepareTransaction(inputsBody string, outputsBody string) string {
 			panic(err)
 		}
 
-		keys[i] = k
 		newTransaction.PushInput(inputHash)
 	}
 
@@ -108,14 +138,29 @@ func PrepareTransaction(inputsBody string, outputsBody string) string {
 		newTransaction.PushOutput(addr, out.Coins, out.Hours)
 	}
 
-	newTransaction.SignInputs(keys)
+	if signatureList == nil {
+		newTransaction.SignInputs(keys)
+	} else {
+		newTransaction.Sigs = make([]cipher.Sig, len(signatureList))
+		for i, sig := range signatureList {
+			decodedSig, e := hex.DecodeString(sig)
+			if e != nil {
+				panic(e)
+			}
+
+			s := cipher.Sig{}
+			if len(decodedSig) != len(s) {
+				log.Panic("Invalid signature length")
+			}
+			copy(s[:], decodedSig[:])
+			newTransaction.Sigs[i] = s
+		}
+	}
 	newTransaction.UpdateHeader()
 
 	if err := newTransaction.Verify(); err != nil {
 		panic(err)
 	}
 
-	d := newTransaction.Serialize()
-
-	return hex.EncodeToString(d)
+	return newTransaction
 }
